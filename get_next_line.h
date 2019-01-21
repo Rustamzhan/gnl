afeebad4c92f290f3d7532898c8bb2f41016f019
astanton <astanton@student.21-school.ru>
2019-01-21 16:31:47 +0300

    1

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astanton <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/14 21:02:27 by astanton          #+#    #+#             */
+/*   Updated: 2019/01/16 04:04:23 by astanton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+
+# define BUFF_SIZE 100
+
+int	get_next_line(const int fd, char **line);
+
+#endif
